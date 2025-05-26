836e9cc670117df8c42be4d2831f3d1957e02329
24dw.francisco.arrebola@arangoya.net <24dw.francisco.arrebola@arangoya.net>
2025-05-26 16:50:02 +0200

    Version: 0.0.45

--- Supermercado/src/vista/VentanaInicio.java
+++ Supermercado/src/vista/VentanaInicio.java
@@ -1,14 +1,12 @@
 package vista;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
+import javax.swing.JOptionPane;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
 import controlador.Main;
-
+import javax.swing.JPasswordField;
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
@@ -23,7 +21,7 @@ public class VentanaInicio extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField textField;
-	private JTextField textField_1;
+	private JPasswordField passwordField;
 
 	public VentanaInicio() {
 		Main.resetearCompra();
@@ -81,32 +79,47 @@ public class VentanaInicio extends JFrame {
 		gbc_lblNewLabel_2.gridy = 6;
 		contentPane.add(lblNewLabel_2, gbc_lblNewLabel_2);
 		
-		textField_1 = new JTextField();
-		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_1.gridx = 1;
-		gbc_textField_1.gridy = 7;
-		contentPane.add(textField_1, gbc_textField_1);
-		textField_1.setColumns(10);
+		passwordField = new JPasswordField();
+		GridBagConstraints gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.insets = new Insets(0, 0, 5, 5);
+		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordField.gridx = 1;
+		gbc_passwordField.gridy = 7;
+		contentPane.add(passwordField, gbc_passwordField);
+		passwordField.setColumns(10);
 		
 		JButton btnNewButton = new JButton("Iniciar sesión");
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				String usuario = textField.getText();
-				String password = textField_1.getText();
+				String password = new String(passwordField.getPassword());
 				String login = Main.login(usuario, password);
-				if(login.equals("Usuario validado")) {
-					Main.inicioSesion();
-				}
-				else if(login.equals("Contraseña incorrecta")) {
-					lblEstadoSesion.setText(login);
-					textField_1.setText("");
-					
-				}
-				else {
-					lblEstadoSesion.setText(login);
+
+				switch (login) {
+					case "Usuario validado":
+						Main.inicioSesion();
+						break;
+					case "Contraseña incorrecta":
+						JOptionPane.showMessageDialog(VentanaInicio.this, "Contraseña incorrecta.", "Error de inicio de sesión", JOptionPane.ERROR_MESSAGE);
+						passwordField.setText("");
+						break;
+					case "Usuario no encontrado":
+						JOptionPane.showMessageDialog(
+							VentanaInicio.this,
+							"Usuario no encontrado.",
+							"Error de inicio de sesión",
+							JOptionPane.ERROR_MESSAGE
+						);
+						break;
+					default:
+						JOptionPane.showMessageDialog(
+							VentanaInicio.this,
+							login,
+							"Error de inicio de sesión",
+							JOptionPane.ERROR_MESSAGE
+						);
+						break;
 				}
 			}
 		});
